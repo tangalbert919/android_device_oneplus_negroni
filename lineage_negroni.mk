tangalbert919/android_device_oneplus_lemonadep

negroni: Initial device tree conversion
Change-Id: I00cc88caf33b0d8c51e236e123302f7da23fa539

lineage_negroni.mk
@@ -18,25 +18,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonadep device
-$(call inherit-product, device/oneplus/lemonadep/device.mk)
+# Inherit from negroni device
+$(call inherit-product, device/oneplus/negroni/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonadep
-PRODUCT_DEVICE := lemonadep
+PRODUCT_NAME := lineage_negroni
+PRODUCT_DEVICE := negroni
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2123
+PRODUCT_MODEL := NE2213
 
-PRODUCT_SYSTEM_NAME := OnePlus9Pro_EEA
-PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
+PRODUCT_SYSTEM_NAME := OnePlus10Pro_EEA
+PRODUCT_SYSTEM_DEVICE := OnePlus10Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus9Pro \
-    TARGET_PRODUCT=OnePlus9Pro_EEA
+    TARGET_DEVICE=OnePlus10Pro \
+    TARGET_PRODUCT=OnePlus10Pro_EEA
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9Pro_EEA/OnePlus9Pro:12/SKQ1.210216.001/R.202203102003:user/release-keys
+BUILD_FINGERPRINT := OnePlus/NE2213EEA/OP516FL1:12/SKQ1.211019.001/S.202203262150:user/release-keys

tangalbert919/android_device_oneplus_negroni

lineage_negroni.mk
@@ -8,25 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonadep device
-$(call inherit-product, device/oneplus/lemonadep/device.mk)
+# Inherit from negroni device
+$(call inherit-product, device/oneplus/negroni/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonadep
-PRODUCT_DEVICE := lemonadep
+PRODUCT_NAME := lineage_negroni
+PRODUCT_DEVICE := negroni
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2125
+PRODUCT_MODEL := NE2215
 
-PRODUCT_SYSTEM_NAME := OnePlus9Pro
-PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
+PRODUCT_SYSTEM_NAME := OnePlus10Pro
+PRODUCT_SYSTEM_DEVICE := OnePlus10Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus9Pro \
-    TARGET_PRODUCT=OnePlus9Pro
+    TARGET_DEVICE=OnePlus10Pro \
+    TARGET_PRODUCT=OnePlus10Pro
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9Pro/OnePlus9Pro:12/SKQ1.210216.001/R.202204081955:user/release-keys
+BUILD_FINGERPRINT := OnePlus/NE2215/OP516FL1:12/SKQ1.211019.001/S.202203262150:user/release-keys